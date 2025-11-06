0b7b8351bd5a3bfd70041d7148a2d1f8a5b89ae5
Grace Oyiza <graceoyiza.dev@gmail.com>
2025-11-06 19:49:21 +0000

    fix: update 2-arguments.js to match correct outputformat

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('0 argument');
+} else if (args === 1) {
+  console.log('1 argument');
+} else {
+  console.log(`${args} arguments`);
+}
